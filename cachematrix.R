9f491f4d8d83ef8dc79ea784dcbb461a409bf6c3
datalogyism <datalogyism@gmail.com>
2015-07-20 10:44:03 +0800

    solution for programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These 2 functions allow us to cache matrix inversions
 
-## Write a short comment describing this function
-
+## makeCacheMatrix creates a special matrix, which is really
+## a list containing functions to:
+## set and get the value of the matrix
+## set and get the value of the matrix inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    invx <- NULL
+    set <- function(y) {
+        #store matrix in parent environment
+        x <<- y
+        #invalidate matrix-inverse cache
+        invx <<- NULL
+    }
+    get <- function() {
+        x
+    }
+    setinv <- function(inv = matrix()) {
+        #Store calculated inverse into cache (in parent environment)
+        invx <<- inv
+    }
+    getinv <- function() {
+        invx
+    }
+    list(
+        set = set,
+        get = get,
+        setinv = setinv,
+        getinv = getinv
+    )
 }
 
-
-## Write a short comment describing this function
-
+## The following function calculates the inverse of the
+## special "matrix" created by the above function.
+## It first checks to see if the inverse has already been
+## calculated, returning "cached" version if so.
+## Otherwise, the inverse is computed and assigned via setinv
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # Retrieve cached matrix inverse (if it exists)
+    invx <- x$getinv()
+    if (!is.null(invx)) {
+        # Cache is not empty, return cached inverse
+        message("getting cached data")
+        return(invx)
+    }
+    # Cache is empty, we'll need to compute the inverse
+    data <- x$get()
+    invx <- solve(data, ...)
+    # Store inverse into cache
+    x$setinv(invx)
+    invx
 }
